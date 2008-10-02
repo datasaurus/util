6b50e3f1a4fd13ca0b450d685d165a7bc7383fae
Gordon Carrie <dev0@trekix.net>
2008-10-02 21:09:45 +0000

    Added a doomed hash_add call.  This exercises hash_add and the error message facility.

--- test/hash3.sh
+++ test/hash3.sh
@@ -2,7 +2,7 @@
 
 # This script tests the hash table interface in visky3.
 #
-# $Id: hash3.sh,v 1.1 2008/10/01 21:18:08 gcarrie Exp $
+# $Id: hash3.sh,v 1.2 2008/10/01 21:34:02 gcarrie Exp $
 
 # This test uses hash3.c.  The driver application creates a small
 # hash table and then clears it one entry at a time while printing
@@ -22,10 +22,14 @@ foo -> 0
 bar -> 2
 hello -> 10
 world -> 11
+Modifying table recklessly.
+Failed to add entry for "bar".  Error message was:
+bar in use.
+
 Clearing table.
 END
 COPT='-g -Wall -Wmissing-prototypes -Isrc/ -DMEM_DEBUG'
-if cc $COPT -o hash hash3.c src/hash.c src/alloc.c
+if cc $COPT -o hash hash3.c src/hash.c src/err_msg.c src/alloc.c
 then
     echo 'Running app from hash3.c with memory trace going to memtrace.'
     ./hash > attempt 2> memtrace
