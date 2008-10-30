23e14d74c17857def4db9b12355dfc2a6443f790
Gordon Carrie <dev0@trekix.net>
2008-10-30 15:14:38 +0000

    Moved multi-dimension allocators to separate files

--- src/alloc.h
+++ src/alloc.h
@@ -9,7 +9,7 @@
  *
  * Please send feedback to user0@tkgeomap.org
  *
- * @(#) $Id: alloc.h,v 1.1 2008/09/01 17:55:10 tkgeomap Exp $
+ * @(#) $Id: alloc.h,v 1.2 2008/10/30 15:08:33 gcarrie Exp $
  *
  **********************************************************************
  *
@@ -39,11 +39,7 @@ void *realloc_mdb(void *m, size_t sz);
 void *malloc_mdb(size_t sz, char *fl_nm, int ln);
 void *realloc_mdb(void *m, size_t sz, char *fl_nm, int ln);
 void free_mdb(void *m, char *fl_nm, int ln);
-float ***Alloc_Arr3(size_t i, size_t j, size_t k);
-void Alloc_Free3(float ***d);
-float ****Alloc_Arr4(size_t i, size_t j, size_t k, size_t l);
-void Alloc_Free4(float ****d);
-    
+
 #ifdef __cplusplus
 }
 #endif
