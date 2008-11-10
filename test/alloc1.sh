5bb4275abf1f1625ddc24d2c8f125561802aba56
Gordon Carrie <dev0@trekix.net>
2008-11-10 03:12:34 +0000

    Added test for unusable diagnostic file

--- test/alloc1.sh
+++ test/alloc1.sh
@@ -2,7 +2,7 @@
 #
 # This script exercises alloc1.c.
 #
-# $Id: alloc1.sh,v 1.1 2008/10/31 21:04:41 gcarrie Exp $
+# $Id: alloc1.sh,v 1.2 2008/11/06 18:24:42 gcarrie Exp $
 
 EXEC=alloc1
 FINDLEAKS=src/findleaks
@@ -13,6 +13,7 @@ $RM $OUT
 
 echo test1: building and running alloc1
 cc -Isrc -o $EXEC src/alloc.c alloc1.c
+echo Starting test1
 $EXEC
 echo Done with test1
 echo ""
@@ -22,6 +23,7 @@ echo test2: building and running alloc1 with memory trace.
 echo An account of allocations and calls to free should appear on terminal
 export MEM_DEBUG=2
 cc -Isrc -o alloc1 src/alloc.c alloc1.c
+echo Starting test2
 $EXEC
 echo Done with test2
 echo ""
@@ -33,6 +35,7 @@ echo YOU SHOULD LOOK AT $OUT
 echo Contents of $OUT should resemble output from test2
 export MEM_DEBUG=$OUT
 cc -Isrc -o alloc1 src/alloc.c alloc1.c
+echo Starting test3
 $EXEC
 echo Done with test3
 echo ""
@@ -42,7 +45,22 @@ echo test4: building and running alloc1.
 echo Sending memory trace to findleaks, which should not find anything.
 export MEM_DEBUG=2
 cc -Isrc -o alloc1 src/alloc.c alloc1.c
+echo Starting test4
 $EXEC 2>&1 | $FINDLEAKS
 echo Done with test4
 echo ""
 $RM $EXEC
+
+echo test5: building and running alloc1.
+echo Sending memory trace to an unusable file.
+echo There should be no diagnostic output, only a warning.
+OUT2=lock
+export MEM_DEBUG=$OUT2
+cc -Isrc -o alloc1 src/alloc.c alloc1.c
+touch $OUT2
+chmod 444 $OUT2
+echo Starting test5
+$EXEC
+echo Done with test5
+echo ""
+$RM $EXEC $OUT2
