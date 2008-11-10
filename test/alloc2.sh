1a1fbe6c24cde50448e7b6aca780bd6598d9e168
Gordon Carrie <dev0@trekix.net>
2008-11-10 05:26:55 +0000

    Added verbosity

--- test/alloc2.sh
+++ test/alloc2.sh
@@ -2,7 +2,7 @@
 #
 # This script exercises alloc2.c.
 #
-# $Id: alloc2.sh,v 1.2 2008/11/06 18:24:42 gcarrie Exp $
+# $Id: alloc2.sh,v 1.3 2008/11/10 05:25:09 gcarrie Exp $
 
 EXEC=alloc2
 FINDLEAKS=src/findleaks
@@ -13,6 +13,7 @@ $RM $OUT
 
 echo test1: building and running alloc2
 cc -Isrc -o $EXEC src/alloc.c alloc2.c
+echo Starting test1
 $EXEC
 echo Done with test1
 echo ""
@@ -22,6 +23,7 @@ echo test2: building and running alloc2 with memory trace.
 echo An account of allocations and calls to free should appear on terminal
 export MEM_DEBUG=2
 cc -Isrc -o alloc2 src/alloc.c alloc2.c
+echo Starting test2
 $EXEC
 echo Done with test2
 echo ""
@@ -32,6 +34,7 @@ echo test3: building and running alloc2.
 echo Sending memory trace to findleaks, which should report a leak.
 export MEM_DEBUG=2
 cc -Isrc -o alloc2 src/alloc.c alloc2.c
+echo Starting test3
 $EXEC 2>&1 | $FINDLEAKS
 echo Done with test3
 echo ""
