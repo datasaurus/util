5702dbe191371cb1960403c3db53e857a37e6995
Gordon Carrie <dev0@trekix.net>
2008-11-11 21:09:27 +0000

    Put text of test application into test script

--- test/hash2.sh
+++ test/hash2.sh
@@ -2,15 +2,64 @@
 
 # This script tests the hash table interface in visky3.
 #
-# $Id: hash2.sh,v 1.3 2008/10/02 21:18:29 gcarrie Exp $
-
-# This test uses hash2.c.  The driver application creates a small
-# hash table and modifies it.  It also prints a memory trace.
+# $Id: hash2.sh,v 1.4 2008/10/30 21:11:34 gcarrie Exp $
 
 # This is the remove command.  Change this to : to retain intermediate results.
 
 RM='rm -f'
 FINDLEAKS=src/findleaks
+
+# The test application creates a small hash table and modifies it.  It also
+# prints a memory trace.
+
+# Here is the source code for the test application.
+
+cat > hash2.c << END
+#include <stdio.h>
+#include <hash.h>
+
+void hash_print(struct hash_tbl *tblP, char *key);
+
+void hash_print(struct hash_tbl *tblP, char *key)
+{
+    unsigned n;
+
+    if (hash_get(tblP, key, &n)) {
+	printf("%s -> %d\n", key, n);
+    } else {
+	fprintf(stderr, "Entry for %s disappeared from table.\n", "foo");
+    }
+}
+
+int main(void)
+{
+    struct hash_tbl tbl;
+    int n;
+    char *keys[] = {
+	"foo", "bar", "hello", "world"
+    };
+
+    hash_init(&tbl, 4);
+    hash_set(&tbl, "foo", 0);
+    hash_set(&tbl, "bar", 1);
+    hash_set(&tbl, "hello", 10);
+    hash_set(&tbl, "world", 11);
+    for (n = 0; n < 4; n++) {
+	hash_print(&tbl, keys[n]);
+    }
+
+    printf("Modifying table.\n");
+    hash_set(&tbl, "bar", 2);
+    for (n = 0; n < 4; n++) {
+	hash_print(&tbl, keys[n]);
+    }
+
+    hash_clear(&tbl);
+    return 0;
+}
+END
+
+# Output from the application should match the contents of file correct.
 
 cat << END > correct
 foo -> 0
@@ -23,6 +72,9 @@ bar -> 2
 hello -> 10
 world -> 11
 END
+
+# Build, run, and evaluate the test application
+
 COPT='-g -Wall -Wmissing-prototypes -Isrc/ -DMEM_DEBUG'
 if cc $COPT -o hash hash2.c src/hash.c src/err_msg.c src/alloc.c
 then
@@ -46,6 +98,6 @@ echo 'Memory report (will not say anything if no leaks)'
 $FINDLEAKS < memtrace
 echo 'Memory check done'
 
-$RM correct memtrace
+$RM correct memtrace hash2.c
 
 echo 'Done with hash2 test'
