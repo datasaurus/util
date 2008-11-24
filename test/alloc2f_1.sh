8173878f754d0c401677f145a227fe6009c205ea
Gordon Carrie <dev0@trekix.net>
2008-11-24 02:30:24 +0000

    Initial revision

---
+++ test/alloc2f_1.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+#
+#- alloc2f_1.sh --
+#-	This script tests the allocators defined in src/alloc2f.c
+#-	It allocates, accesses, and then frees a two dimensional array.
+#-
+# Copyright (c) 2008 Gordon D. Carrie
+# Licensed under the Open Software License version 3.0
+#
+# Please send feedback to user0@tkgeomap.org
+#
+# $Id: $
+#
+########################################################################
+
+# This is the remove command.  Change this to : to retain intermediate results.
+
+RM='rm -f'
+#RM=:
+
+CFLAGS="-g -Wall -Wmissing-prototypes"
+
+# Here is the source code for the driver application.
+
+cat > alloc2f_1.c << END
+#include <stdio.h>
+#include <err_msg.h>
+#include <alloc2f.h>
+
+int main(void)
+{
+    long jmax, imax;
+    long j, i;
+    float *p, *q;
+    float **dat = NULL, **p2, **q2;
+
+    jmax = 100;
+    imax = 100;
+
+    dat = calloc2f(jmax, imax);
+    if ( !dat ) {
+	fprintf(stderr, "Could not allocate dat\n%s\n", err_get());
+	return 1;
+    }
+    for (p2 = dat, q2 = dat + 1, j = 0; *q2 ; p2++, q2++) {
+	j = p2 - dat;
+	for (p = *p2, q = *q2, i = 0; p < q; p++) {
+	    i = p - *p2;
+	    *p = 10 * j + i;
+	}
+    }
+    printf("dat[1][1] = %8.1f\n", dat[1][1]);
+    printf("dat[9][9] = %8.1f\n", dat[9][9]);
+    printf("dat[jmax-1][imax-1] = %8.1f\n", dat[jmax-1][imax-1]);
+    free2f(dat);
+    return 0;
+}
+END
+
+cc -Isrc -o alloc2f_1 alloc2f_1.c src/alloc2f.c src/alloc.c src/err_msg.c
+
+echo test1: building and running alloc2f_1
+echo ""
+echo Starting test1
+alloc2f_1
+echo Done with test1
+echo ""
+
+echo test2: building and running allocf1 with memory trace.
+echo An account of allocations and calls to free should appear on terminal
+echo ""
+echo Starting test2
+export MEM_DEBUG=2
+alloc2f_1
+echo Done with test2
+echo ""
+unset MEM_DEBUG
+
+echo test3: building and running alloc2f_1.
+echo Sending memory trace to findleaks, which should not find anything.
+export MEM_DEBUG=2
+echo ""
+echo Starting test3
+alloc2f_1 2>&1 | src/findleaks
+echo Done with test3
+echo ""
+unset MEM_DEBUG
+
+echo test4: simulate allocation failure in alloc2f_1
+echo This should produce a warning about failure to allocate dat.
+export MEM_FAIL="src/alloc2f.c:41"
+echo ""
+echo Starting test4
+alloc2f_1
+echo Done with test4
+echo ""
+unset MEM_FAIL
+
+echo test5: simulate a later allocation failure in alloc2f_1
+echo This should produce a warning about failure to allocate dat.
+export MEM_FAIL="src/alloc2f.c:46"
+echo ""
+echo Starting test5
+alloc2f_1
+echo Done with test5
+echo ""
+unset MEM_FAIL
+
+echo test6: simulate later allocation failure in alloc2f_1 with memory tracing
+echo This should produce a warning about failure to allocate dat.
+echo Trace output should show no leaks
+export MEM_FAIL="src/alloc2f.c:46"
+export MEM_DEBUG=3
+echo ""
+echo Starting test6
+alloc2f_1 3>&1
+echo Done with test6
+echo ""
+unset MEM_FAIL
+
+$RM alloc2f_1.c alloc2f_1
