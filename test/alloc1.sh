30f06b2442c8940c2a464634fde92c0d16af2ea5
Gordon Carrie <dev0@trekix.net>
2008-11-27 05:42:36 +0000

    Removed spurious clean up commands. Consolidated compile lines.

--- test/alloc1.sh
+++ test/alloc1.sh
@@ -9,15 +9,17 @@
 #
 # Please send feedback to user0@tkgeomap.org
 #
-# $Id: alloc1.sh,v 1.7 2008/11/24 02:28:10 gcarrie Exp $
+# $Id: alloc1.sh,v 1.8 2008/11/25 22:51:20 gcarrie Exp $
 #
 ########################################################################
 
 # This is the remove command.  Change this to : to retain intermediate results.
 
-RM='rm -f'
+RM=:
+#RM='rm -f'
 
-$RM alloc1.out
+CC="cc"
+CFLAGS="-g -Wall -Wmissing-prototypes"
 
 # Here is the source code for the driver application.
 cat > alloc1.c << END
@@ -57,29 +59,30 @@ int main(void)
 }
 END
 
+if ! $CC $CFLAGS -Isrc -o alloc1 src/alloc.c alloc1.c
+then
+    echo "Could not compile the test application"
+    exit 1
+fi
+
 echo test1: building and running alloc1
-cc -Isrc -o alloc1 src/alloc.c alloc1.c
 echo Starting test1
 alloc1
 echo Done with test1
 echo ""
-$RM alloc1
 
 echo test2: building and running alloc1 with memory trace.
 echo An account of allocations and calls to free should appear on terminal
 export MEM_DEBUG=2
-cc -Isrc -o alloc1 src/alloc.c alloc1.c
 echo Starting test2
 alloc1
 echo Done with test2
 echo ""
-$RM alloc1
 unset MEM_DEBUG
 
 echo test3: building and running alloc1 with memory trace.
 echo An account of allocations and calls to free should appear in alloc1.out
 export MEM_DEBUG=alloc1.out
-cc -Isrc -o alloc1 src/alloc.c alloc1.c
 echo Starting test3
 alloc1
 echo Here are the contents of alloc1.out
@@ -88,13 +91,12 @@ echo probably different.
 cat alloc1.out
 echo Done with test3
 echo ""
-$RM alloc1 alloc1.out
+$RM alloc1.out
 unset MEM_DEBUG
 
 echo test4: building and running alloc1.
 echo Sending memory trace to findleaks, which should not find anything.
 export MEM_DEBUG=2
-cc -Isrc -o alloc1 src/alloc.c alloc1.c
 echo Starting test4
 if alloc1 2>&1 | src/findleaks
 then
@@ -104,14 +106,12 @@ else
 fi
 echo Done with test4
 echo ""
-$RM alloc1
 unset MEM_DEBUG
 
 echo test5: building and running alloc1.
 echo Sending memory trace to an unwritable file.
 echo There should be no diagnostic output, only a warning.
 export MEM_DEBUG=alloc1.out
-cc -Isrc -o alloc1 src/alloc.c alloc1.c
 touch alloc1.out
 chmod 444 alloc1.out
 echo Starting test5
@@ -119,18 +119,16 @@ alloc1
 echo Done with test5
 echo ""
 chmod 644 alloc1.out
-$RM alloc1 alloc1.out
+$RM alloc1.out
 unset MEM_DEBUG
 
 echo test6: simulate allocation failure in alloc1
 echo This should produce a warning about failure to allocate x3.
 export MEM_FAIL="alloc1.c:22"
-cc -Isrc -o alloc1 src/alloc.c alloc1.c
 echo Starting test6
 alloc1
 echo Done with test6
 echo ""
-$RM alloc1
 unset MEM_FAIL
 
-$RM alloc1.c
+$RM alloc1.c alloc1
