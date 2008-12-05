9ad8d233d302fb4c0d812ab71301d43dff3556b9
Gordon Carrie <dev0@trekix.net>
2008-12-05 21:17:40 +0000

    Added tests for big arrays

--- test/alloc2f_2.sh
+++ test/alloc2f_2.sh
@@ -10,7 +10,7 @@
 #
 # Please send feedback to dev0@trekix.net
 #
-# $Id: alloc2f_2.sh,v 1.3 2008/11/27 05:54:34 gcarrie Exp $
+# $Id: alloc2f_2.sh,v 1.4 2008/12/02 17:19:39 gcarrie Exp $
 #
 ########################################################################
 
@@ -37,12 +37,37 @@ int main(void)
 	fprintf(stderr, "Could not allocate x:\n%s\n", err_get());
     }
     free2f(x);
+
+    j_max = (LONG_MAX / 3) * 2;
+    i_max = 1000;
+    x = calloc2f(j_max, i_max);
+    if ( !x ) {
+	fprintf(stderr, "Could not allocate x:\n%s\n", err_get());
+    }
+    free2f(x);
+
+    j_max = 1000;
+    i_max = ULONG_MAX - 1;
+    x = calloc2f(j_max, i_max);
+    if ( !x ) {
+	fprintf(stderr, "Could not allocate x:\n%s\n", err_get());
+    }
+    free2f(x);
+
+    j_max = 1000;
+    i_max = (LONG_MAX / 3) * 2;
+    x = calloc2f(j_max, i_max);
+    if ( !x ) {
+	fprintf(stderr, "Could not allocate x:\n%s\n", err_get());
+    }
+    free2f(x);
+
     return 0;
 }
 END
 
 echo test1: building and running alloc2f_2.
-echo It should complain about non-positive dimensions.
+echo It should complain about excessively large or non-positive dimensions.
 echo ""
 cc -Isrc -o alloc2f_2 alloc2f_2.c src/alloc2f.c src/alloc.c src/err_msg.c
 echo Starting test1
