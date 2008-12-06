704f0aaa4e29dbfa844427ffc1273922673deb5f
Gordon Carrie <dev0@trekix.net>
2008-12-06 15:51:50 +0000

    Added white space and separators to output

--- src/allocfvi.c
+++ src/allocfvi.c
@@ -9,7 +9,7 @@
   
    Please send feedback to dev0@trekix.net
   
-   $Id: allocfvi.c,v 1.8 2008/12/06 06:11:49 gcarrie Exp $
+   $Id: allocfvi.c,v 1.9 2008/12/06 15:45:12 gcarrie Exp $
  */
 
 #include <stdlib.h>
@@ -42,11 +42,13 @@ int main(void)
     }
 
     /* Print a picture showing where things are stored */
-    printf("\njmax = %ld.  imax = %ld\n", jmax, imax);
+    printf("\njmax = %ld.  imax = %ld\n\n", jmax, imax);
     printf("           dat2       %12p\n", dat2);
+    printf("\n");
     for (j = 0; j < jmax; j++) {
 	printf("%p dat2[%ld]    %12p\n", &dat2[j], j, dat2[j]);
     }
+    printf("\n");
     for (j = 0; j < jmax; j++) {
 	for (i = 0; i < imax; i++) {
 	    printf("%p dat2[%ld][%ld] %12.1f\n", &dat2[j][i], j, i, dat2[j][i]);
@@ -74,12 +76,15 @@ int main(void)
     }
 
     /* Print a picture showing where things are stored */
-    printf("\nkmax = %ld.  jmax = %ld.  imax = %ld\n", kmax, jmax, imax);
+    printf("\n------------------------------------------------\n");
+    printf("\nkmax = %ld.  jmax = %ld.  imax = %ld\n\n", kmax, jmax, imax);
     printf("           dat3          %12p\n", dat3);
+    printf("\n");
     for (k = 0; k < kmax; k++) {
 	printf("%p dat3[%ld]       %12p\n", &dat3[k], k, dat3[k]);
     }
     printf("%p dat3[%ld]       %12p\n", &dat3[kmax], kmax, dat3[kmax]);
+    printf("\n");
     for (k = 0; k < kmax; k++) {
 	for (j = 0; j < jmax; j++) {
 	    printf("%p dat3[%ld][%ld]    %12p\n", &dat3[k][j], k, j, dat3[k][j]);
@@ -87,6 +92,7 @@ int main(void)
     }
     printf("%p dat3[%ld][%ld]    %12p\n",
 	    &dat3[kmax-1][jmax], kmax-1, jmax, dat3[kmax-1][jmax]);
+    printf("\n");
     for (k = 0; k < kmax; k++) {
 	for (j = 0; j < jmax; j++) {
 	    for (i = 0; i < imax; i++) {
@@ -120,13 +126,16 @@ int main(void)
     }
 
     /* Print a picture showing where things are stored */
-    printf("\nlmax = %ld.  kmax = %ld.  jmax = %ld.  imax = %ld\n",
+    printf("\n------------------------------------------------\n");
+    printf("\nlmax = %ld.  kmax = %ld.  jmax = %ld.  imax = %ld\n\n",
 	    lmax, kmax, jmax, imax);
     printf("           dat4             %12p\n", dat4);
+    printf("\n");
     for (l = 0; l < lmax; l++) {
 	printf("%p dat4[%ld]          %12p\n", &dat4[l], l, dat4[l]);
     }
     printf("%p dat4[%ld]          %12p\n", &dat4[lmax], lmax, dat4[lmax]);
+    printf("\n");
     for (l = 0; l < lmax; l++) {
 	for (k = 0; k < kmax; k++) {
 	    printf("%p dat4[%ld][%ld]       %12p\n",
@@ -137,6 +146,7 @@ int main(void)
     }
     printf("%p dat4[%ld][%ld]       %12p\n",
 	    &dat4[lmax-1][kmax], lmax-1, kmax, dat4[lmax-1][kmax]);
+    printf("\n");
     for (l = 0; l < lmax; l++) {
 	for (k = 0; k < kmax; k++) {
 	    for (j = 0; j < jmax; j++) {
@@ -151,6 +161,7 @@ int main(void)
 	    &dat4[lmax-1][kmax-1][jmax],
 	    lmax-1, kmax-1, jmax,
 	    dat4[lmax-1][kmax-1][jmax]);
+    printf("\n");
     for (l = 0; l < lmax; l++) {
 	for (k = 0; k < kmax; k++) {
 	    for (j = 0; j < jmax; j++) {
