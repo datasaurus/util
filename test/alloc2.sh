40cef6af3abb16134d9f08377fe3d5c5965f2c89
Gordon Carrie <dev0@trekix.net>
2008-12-07 21:45:15 +0000

    Added lots of explanatory output

--- test/alloc2.sh
+++ test/alloc2.sh
@@ -8,20 +8,45 @@
 #
 # Please send feedback to dev0@trekix.net
 #
-# $Id: alloc2.sh,v 1.9 2008/11/27 05:50:55 gcarrie Exp $
+# $Id: alloc2.sh,v 1.10 2008/12/02 17:19:39 gcarrie Exp $
 #
 ########################################################################
 
+echo "
+alloc2.sh --
+
+This script tests the allocators defined in src/alloc.c and the findleaks
+application.  See alloc (3) and findleaks (1) for more information.
+
+The script creates a test program that allocates and frees memory using
+functions in alloc.c.  The test program intentionally fails to free
+memory that it allocates.  The failures should be apparent in output from
+the program and findleaks.  You must read the output and check for error
+messages and inconsistencies.
+
+The script puts source code for the test program into a source file
+named alloc2.c and creates an executable named alloc2.  The program
+files are normally deleted when the script exits.  To preserve
+program and temporary files for post mortem troubleshooting, set
+RM in the environment to a command that does not delete anything,
+e.g. ':'.
+
+Usage suggestions:
+./alloc2.sh 2>&1 | less
+env RM=: ./alloc2.sh 2>&1 | less
+
+Copyright (c) 2008 Gordon D. Carrie Licensed under the Open Software
+License version 3.0
+
+--------------------------------------------------------------------------------
+"
+
+RM=${RM:-'rm -f'}
 FINDLEAKS=src/findleaks
 CC="cc"
 CFLAGS="-g -Wall -Wmissing-prototypes"
 
-# Set RM to : to save intermediate files
-RM=${RM:-'rm -f'}
-
 # Here is the source code for the driver application.
-# It allocates some memory, and then fails to free it.
-
 cat > alloc2.c << END
 #include <alloc.h>
 
@@ -34,12 +59,9 @@ int main(void)
     x1 = CALLOC(i, sizeof(float));
     FREE(x1);
 
-    x2 = MALLOC(i * sizeof(float));
-    FREE(x2);
-
-    x3 = CALLOC(i, sizeof(float));
+    x2 = CALLOC(i, sizeof(float));
     i = 2000;
-    x3 = REALLOC(x3, i * sizeof(float));
+    x3 = REALLOC(x2, i * sizeof(float));
 
     return 0;
 }
@@ -53,33 +75,41 @@ fi
 
 # Run the tests
 
-echo test1: building and running alloc2
-echo Starting test1
+echo "test1: This is a dry run alloc2.  There should not be any output,
+error messages, or crashes.
+Starting test1"
 alloc2
-echo Done with test1
-echo ""
+echo "Done with test1
 
-echo test2: building and running alloc2 with memory trace.
-echo An account of allocations and calls to free should appear on terminal
+--------------------------------------------------------------------------------
+"
+
+echo "test2: building and running alloc2 with memory trace.
+An account of allocations and calls to free should appear on terminal
+the terminal.  The format of this account is described in alloc (3).
+Starting test2"
 export MEM_DEBUG=2
-echo Starting test2
 alloc2
-echo Done with test2
-echo ""
 unset MEM_DEBUG
+echo "Done with test2
 
-echo test3: building and running alloc2.
-echo Sending memory trace to findleaks, which should report a leak.
+--------------------------------------------------------------------------------
+"
+
+echo "test3: building and running alloc2.
+Sending memory trace to findleaks, which should report a leak.
+Starting test3"
 export MEM_DEBUG=2
-echo Starting test3
 if alloc2 2>&1 | $FINDLEAKS
 then
     echo Program leaks!
 else
     echo ERROR: Failed to find memory leaks!
 fi
-echo Done with test3
-echo ""
 unset MEM_DEBUG
+echo "Done with test3
+
+--------------------------------------------------------------------------------
+"
 
 $RM alloc2.c
