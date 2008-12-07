065095d3b5ce45fe25b6959ff95e9d8071478bf0
Gordon Carrie <dev0@trekix.net>
2008-12-07 19:20:40 +0000

    Added lots of explanatory output

--- test/alloc1.sh
+++ test/alloc1.sh
@@ -9,14 +9,39 @@
 #
 # Please send feedback to dev0@trekix.net
 #
-# $Id: alloc1.sh,v 1.10 2008/11/27 05:54:34 gcarrie Exp $
+# $Id: alloc1.sh,v 1.11 2008/12/02 17:19:39 gcarrie Exp $
 #
 ########################################################################
 
-# This is the remove command.  Change this to : to retain intermediate results.
+echo "
+alloc1.sh --
+
+This script tests the allocators defined in src/alloc.c.  See
+alloc (3) for information about these functions.  The script creates
+a test program that allocates and frees memory using functions in alloc.c.
+It checks for leaks and imposes various failures.  Output varies with
+system.  You must read the output and check for error messages and
+inconsistencies.
+
+The script puts source code for the test program into a source file
+named alloc1.c and creates an executable named alloc1.  Some output
+is also saved into temporary files.  These program and temporary
+files are normally deleted when the script exits.  To preserve
+program and temporary files for post mortem troubleshooting, set
+RM in the environment to a command that does not delete anything,
+e.g. ':'.
+
+Usage suggestions:
+./alloc1.sh 2>&1 | less
+env RM=: ./alloc1.sh 2>&1 | less
+
+Copyright (c) 2008 Gordon D. Carrie Licensed under the Open Software
+License version 3.0
+
+--------------------------------------------------------------------------------
+"
 
 RM=${RM:-'rm -f'}
-
 CC="cc"
 CFLAGS="-g -Wall -Wmissing-prototypes"
 
@@ -64,74 +89,94 @@ then
     exit 1
 fi
 
-echo test1: building and running alloc1
-echo Starting test1
+echo "test1: building and running alloc1.  There should be no output,
+error messages or crashes.
+Starting test1"
 alloc1
-echo Done with test1
-echo ""
+echo "Done with test1
 
-echo test2: building and running alloc1 with memory trace.
-echo An account of allocations and calls to free should appear on terminal
+--------------------------------------------------------------------------------
+"
+
+echo "test2: building and running alloc1 with memory trace, i.e. MEM_DEBUG
+is set.  account of allocations and calls to free should appear on
+the terminal.  The format of this account is described in alloc (3).
+Starting test2"
 export MEM_DEBUG=2
-echo Starting test2
 alloc1
-echo Done with test2
-echo ""
 unset MEM_DEBUG
+echo "Done with test2
 
-echo test3: building and running alloc1 with memory trace.
-echo An account of allocations and calls to free should appear in alloc1.out
-export MEM_DEBUG=alloc1.out
-echo Starting test3
+--------------------------------------------------------------------------------
+"
+
+echo "test3: building and running alloc1 with memory trace going to a
+file.  An account of allocations and calls to free should appear
+in alloc3.out
+Starting test3"
+export MEM_DEBUG=alloc3.out
 alloc1
-echo Here are the contents of alloc1.out
-echo The output should look like that from test2, although the addresses are
-echo probably different.
-cat alloc1.out
-echo Done with test3
-echo ""
-$RM alloc1.out
 unset MEM_DEBUG
+echo "Here are the contents of alloc3.out.  The output should look like that
+from test2, although the addresses are probably different."
+cat alloc3.out
+$RM alloc3.out
+echo "Done with test3
 
-echo test4: building and running alloc1.
-echo Sending memory trace to findleaks, which should not find anything.
+--------------------------------------------------------------------------------
+"
+
+echo "test4: building and running alloc1 with memory trace and leak
+checking.  This time the memory trace is being piped to findleaks,
+which will look for allocations that are never freed.  See findleaks
+(1).  findleaks should not find any leaks.
+Starting test4"
 export MEM_DEBUG=2
-echo Starting test4
 if alloc1 2>&1 | src/findleaks
 then
-    echo findleaks found leaks!
+    echo "findleaks found leaks!"
 else
-    echo findleaks did not find any leaks.
+    echo "findleaks did not find any leaks."
 fi
-echo Done with test4
-echo ""
 unset MEM_DEBUG
+echo "Done with test4
 
-echo test5: building and running alloc1.
-echo Sending memory trace to an unwritable file.
-echo There should be no diagnostic output, only a warning.
-export MEM_DEBUG=alloc1.out
-touch alloc1.out
-chmod 444 alloc1.out
-echo Starting test5
+--------------------------------------------------------------------------------
+"
+
+echo "test5: building and running alloc1 with memory trace.  Sending
+memory trace to an unwritable file.  There should be a message
+announcing failure to produce diagnostic output.
+Starting test5"
+export MEM_DEBUG=alloc5.out
+touch alloc5.out
+chmod 444 alloc5.out
 alloc1
-echo Done with test5
-echo ""
-chmod 644 alloc1.out
-$RM alloc1.out
+chmod 644 alloc5.out
+$RM alloc5.out
 unset MEM_DEBUG
+echo "Done with test5
 
-echo test6: simulate allocation failures in alloc1
-echo Each sub-test should produce a warnings about failure to allocate x3.
-echo Starting test6
+--------------------------------------------------------------------------------
+"
+
+echo "test6: simulate allocation failures in alloc1 This test will run
+alloc1 several times with the MEM_FAIL environment variable set.
+Each time it will impose an apparent allocation failure at a different
+place in the program.  Each time it runs the program should report the
+failure and exit gracefully.
+Starting test6"
 for n in `egrep -n 'MALLOC|CALLOC|REALLOC' alloc1.c | sed 's/:.*//'`
 do
-    echo "Simulating allocation failure at alloc1.c:$n"
+    echo "Running alloc1, simulating allocation failure at alloc1.c:$n"
     export MEM_FAIL="alloc1.c:$n"
     alloc1
     unset MEM_FAIL
 done
-echo Done with test6
-echo ""
+echo "Done with test6
+
+--------------------------------------------------------------------------------
+"
 
 $RM alloc1.c alloc1
+echo "$0 all done."
