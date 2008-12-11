32c75885c62bd8bb424b9098a4f5f4c063a53bec
Gordon Carrie <dev0@trekix.net>
2008-12-11 19:48:12 +0000

    Shortened and clarified output

--- test/chkalloc.sh
+++ test/chkalloc.sh
@@ -8,7 +8,7 @@
 #
 # Please send feedback to dev0@trekix.net
 #
-# $Id: $
+# $Id: chkalloc.sh,v 1.1 2008/12/11 17:37:41 gcarrie Exp $
 #
 ########################################################################
 
@@ -17,6 +17,8 @@ chkalloc.sh --
 
 This script tests the src/chkalloc application.  See chkalloc (1) for
 information on chkalloc.
+
+To save temporary files, set RM to : in environment before running this script.
 
 Copyright (c) 2008 Gordon D. Carrie
 Licensed under the Open Software License version 3.0
@@ -34,33 +36,27 @@ CFLAGS="-g -Wall -Wmissing-prototypes"
 
 echo "test1: evaluate output from a normal process."
 
+result=success
+correct=0
 $FINDLEAKS > test1.out << END
 0x00000001 (000000001) allocated at makebelieve.c:10
 0x00000001 (000000002) freed at makebelieve.c:14
 0x00000002 (000000003) allocated at makebelieve.c:16
 0x00000002 (000000004) freed at makebelieve.c:20
 END
-case $? in
-    0)
-	echo "chkalloc reported no leaks."
-	result=success
-	;;
-    1)
-	echo "chkalloc reported leaks when there were not any."
-	result=fail
-	;;
-    -1)
-	echo "chkalloc did not see input."
-	result=fail
-	;;
-esac
-cat /dev/null > correct2.out
-if diff test1.out correct2.out
+if [ $? -eq $correct ]
 then
-    echo "chkalloc produced correct output."
-    result=success
+    echo "chkalloc gave correct return value ($correct)"
 else
-    echo "chkalloc produced incorrect output."
+    echo "chkalloc gave wrong return value ($? instead of $correct)"
+    result=fail
+fi
+cat /dev/null > correct1.out
+if diff test1.out correct1.out
+then
+    echo "chkalloc output was correct."
+else
+    echo "chkalloc output was wrong."
     result=fail
 fi
 echo "test1 result = $result"
@@ -72,31 +68,25 @@ echo "Done with test1
 
 echo "test2: evaluate output from a process that leaks memory."
 
+result=success
+correct=1
 $FINDLEAKS > test2.out << END
 0x00000001 (000000001) allocated at makebelieve.c:10
 0x00000001 (000000002) freed at makebelieve.c:14
 0x00000002 (000000003) allocated at makebelieve.c:16
 END
-case $? in
-    0)
-	echo "chkalloc failed to report a leak."
-	result=fail
-	;;
-    1)
-	echo "chkalloc reported leaks."
-	result=success
-	;;
-    -1)
-	echo "chkalloc did not see input."
-	result=fail
-	;;
-esac
+if [ $? -eq $correct ]
+then
+    echo "chkalloc gave correct return value ($correct)"
+else
+    echo "chkalloc gave wrong return value ($? instead of $correct)"
+    result=fail
+fi
 if egrep -q "Leak at [0-9x]+" test2.out
 then
-    echo "chkalloc produced correct output."
-    result=success
+    echo "chkalloc output was correct."
 else
-    echo "chkalloc produced incorrect output."
+    echo "chkalloc output was wrong."
     result=fail
 fi
 echo "test2 result = $result"
@@ -108,26 +98,22 @@ echo "Done with test2
 
 echo "test3: make sure chkalloc complains if there is no input"
 
+result=success
+correct=2
 : | $FINDLEAKS > test3.out 2>&1
-case $? in
-    0)
-	echo "chkalloc reported leak status with no input."
-	result=fail
-	;;
-    1)
-	echo  "chkalloc reported leak status with no input."
-	result=fail
-	;;
-    -1)
-	echo "chkalloc did not see input."
-	;;
-esac
+if [ $? -eq $correct ]
+then
+    echo "chkalloc gave correct return value ($correct)"
+else
+    echo "chkalloc gave wrong return value ($? instead of $correct)"
+    result=fail
+fi
 echo "Warning chkalloc did not receive input." > correct3.out
 if diff test3.out correct3.out
 then
-    echo "chkalloc produced correct output."
+    echo "chkalloc output was correct."
 else
-    echo "chkalloc produced incorrect output."
+    echo "chkalloc output was wrong."
     result=fail
 fi
 echo "test3 result = $result"
