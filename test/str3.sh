5dae150f65b92b7e0a10bd7cd4b5afbfe90c0c12
Gordon Carrie <dev0@trekix.net>
2009-12-31 00:46:35 +0000

    Updated for new signature in Str_Words

--- test/str3.sh
+++ test/str3.sh
@@ -8,7 +8,7 @@
 #
 # Please send feedback to dev0@trekix.net
 #
-# $Revision: 1.1 $ $Date: 2009/12/26 03:19:00 $
+# $Revision: 1.2 $ $Date: 2009/12/30 23:21:29 $
 #
 ########################################################################
 
@@ -48,6 +48,7 @@ int main(void)
 	strcpy(s, *p);
 	if ( !(words = Str_Words(s, &n_words)) ) {
 	    printf("test %ld failed.\n%s\n", p - s1 + 1, Err_Get());
+	    exit(EXIT_FAILURE);
 	} else {
 	    printf("test %ld: \"%s\" found %d words:\n",
 		    p - s1 + 1, s1[p - s1], n_words);
@@ -63,6 +64,75 @@ int main(void)
 }
 END
 
+# This should be the output
+cat > correct << END
+test 1: "Now is the time ..." found 5 words:
+|Now|
+|is|
+|the|
+|time|
+|...|
+
+test 2: "Now is the time ... " found 5 words:
+|Now|
+|is|
+|the|
+|time|
+|...|
+
+test 3: " Now is the time ..." found 5 words:
+|Now|
+|is|
+|the|
+|time|
+|...|
+
+test 4: " Now is the time ... " found 5 words:
+|Now|
+|is|
+|the|
+|time|
+|...|
+
+test 5: "I have a "lovely bunch" of coconuts." found 6 words:
+|I|
+|have|
+|a|
+|lovely bunch|
+|of|
+|coconuts.|
+
+test 6: "I have a "lovely bu"nch of coconuts." found 6 words:
+|I|
+|have|
+|a|
+|lovely bunch|
+|of|
+|coconuts.|
+
+test 7: "I have a lov"ely bunch" of coconuts." found 6 words:
+|I|
+|have|
+|a|
+|lovely bunch|
+|of|
+|coconuts.|
+
+test 8: "I have "" coconuts." found 4 words:
+|I|
+|have|
+||
+|coconuts.|
+
+test 9: " " found 0 words:
+
+test 10: "	" found 0 words:
+
+test 11: "" found 0 words:
+
+END
+
+# Build the test application
 CFLAGS="-g -Wall -Wmissing-prototypes"
 if ! cc ${CFLAGS} -o str3 str3.c src/str.c src/err_msg.c src/alloc.c
 then
@@ -70,12 +140,27 @@ then
     $RM str3.c str
     exit 1
 fi
-if ./str3
-then
-    echo 'Test application succeeded.'
-else
-    echo 'Test application FAILED.'
-fi
 
-$RM str3.c str3
+# Normal run, checking for leaks with chkalloc.  See alloc (3) and chkalloc (3).
+export MEM_DEBUG=2
+if ! ./str3 > attempt 2> memtrace
+then
+    echo 'Test application failed to run.'
+    exit 1
+fi
+if diff correct attempt
+then
+    echo "string driver produced correct output"
+    $RM attempt hash
+else
+    echo "string driver failed!"
+    exit 1
+fi
+echo ""
+
+echo 'Checking memory trace (will not say anything if no leaks)'
+src/chkalloc < memtrace
+echo 'Memory check done'
+
+$RM str3.c str3 attempt correct memtrace
 echo 'Done with str3 test'
