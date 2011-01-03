ffd6fb15445c5dcda7018c87232e5fd250c81039
Gordon Carrie <dev0@trekix.net>
2011-01-03 20:44:08 +0000

    Documentation is now in man page.

--- src/dyn_alloc.c
+++ src/dyn_alloc.c
@@ -1,13 +1,13 @@
 /*
    -	dyn_alloc.c --
-   -		This file defines dynamic memory allocators.
+   -		This file defines dynamic memory allocators. See dyn_alloc (3).
    -	
    .	Copyright (c) 2010 Gordon D. Carrie
    .	All rights reserved
    .
    .	Please send feedback to dev0@trekix.net
    .
-   .	$Revision: $ $Date: $
+   .	$Revision: 1.1 $ $Date: 2011/01/03 20:30:07 $
  */
 
 #include <string.h>
@@ -15,17 +15,6 @@
 #include <errno.h>
 #include "alloc.h"
 #include "dyn_alloc.h"
-
-/*
-   Copy len bytes from src to base + offset. base should have space for sz0
-   bytes. If offset is too large for pointer arithmetic, the function returns
-   NULL with errno will be set to EOVERFLOW. If offset + len <= sz0, the
-   function just copies the bytes and returns base. If offset + len > sz0, the
-   function attempts to change the allocation at base to offset + len + extra
-   bytes with a call to REALLOC. If REALLOC succeeds, it copies the bytes to
-   the possibly new address.  If REALLOC is called, this function returns the
-   return value from REALLOC, which might also set errno.
- */
 
 void *dyn_alloc(void *base, size_t sz0, size_t offset, void *src, size_t len,
 	size_t extra)
