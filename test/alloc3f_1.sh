8de9a08ee26d92725f2a66fe5ddee501380af2ad
Gordon Carrie <dev0@trekix.net>
2011-11-28 16:09:55 +0000

    Added copyright notice and distribution license

--- test/alloc3f_1.sh
+++ test/alloc3f_1.sh
@@ -5,12 +5,32 @@
 #-	It examines a process that allocates, accesses, and then frees a three
 #-	dimensional array.
 #-
-# Copyright (c) 2008 Gordon D. Carrie
-# All rights reserved
+# Copyright (c) 2011, Gordon D. Carrie. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Please send feedback to dev0@trekix.net
-#
-# $Revision: 1.34 $ $Date: 2009/09/25 21:33:13 $
+# $Revision: 1.35 $ $Date: 2009/10/07 17:06:47 $
 #
 ########################################################################
 
@@ -33,6 +53,7 @@ env JMAX=100 IMAX=100 ./alloc3f_1.sh 2>&1 | less
 
 Copyright (c) 2008 Gordon D. Carrie
 All rights reserved
+See $0 for distribution terms
 
 ################################################################################
 "
